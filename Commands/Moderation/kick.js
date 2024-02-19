a73a70fbb916eb6f651e841e93273e8b7fc6e4f0
NsGames07 <106678739+DevNsGames07@users.noreply.github.com>
2024-02-19 10:15:44 +0100

    Kick System

---
+++ Commands/Moderation/kick.js
@@ -0,0 +1,56 @@
+const { SlashCommandBuilder, EmbedBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle } = require('discord.js');
+const Language = require("../../Schema/Languages")
+const i18next = require('i18next');
+const fs = require('fs');
+
+i18next.init({
+    lng: process.env.defaultLanguage,
+    fallbackLng: process.env.defaultLanguage,
+    resources: {
+        en: {
+            translation: JSON.parse(fs.readFileSync('./Locales/en.json', 'utf8')),
+        },
+    },
+});
+
+module.exports = {
+   data: new SlashCommandBuilder()
+	.setName('kick')
+	.setDescription('Kick a user from the server!')
+    .addUserOption(option =>
+        option.setName("user")
+        .setDescription("Select a users")
+        .setRequired(true)
+    )
+    .addStringOption(option =>
+        option.setName("reason")
+        .setDescription("Give a reason")
+        .setRequired(false)
+    ),
+   async execute(interaction, client) {
+
+    const { guildId, guild } = interaction;
+    
+    const data = await Language.findOne({ Guild: guildId });
+    const language = data && data.Language in i18next.options.resources ? data.Language : process.env.defaultLanguage;
+    i18next.changeLanguage(language);
+
+    
+    const person = interaction.options.getUser("user")
+    const reasonkick = interaction.options.getString("reason") || "No reason provided"
+
+        const embed1 = new EmbedBuilder()
+        .setTitle("**Kick Systeem**")
+        .setThumbnail(person.user.displayAvatarURL({ dynamic: true }))
+        .addFields(
+            { name: `Persoon:`, value: `${person}`, inline: true },
+            { name: `Reason:`, value: `${reasonkick}`, inline: true }
+        )
+        .setColor("Blue")
+        .setFooter({ text: `Powered by ${client.user.username}`, iconURL: client.user.displayAvatarURL({ dynamic: true }) })
+        .setTimestamp();
+
+
+
+   },
+};
